cb9b2288512ad3906a28787a07188ac4ead82fc6
riyaharchandani <riyaharchandani76@gmail.com>
2026-07-08 15:25:50 +0530

    edited

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,44 +1,51 @@
-import { FaGithub, FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/fa"
-import logo from "../assets/riya_harchandaniLogo2.png"
-import { FaSquareXTwitter } from "react-icons/fa6"
+import { FaGithub, FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import logo from "../assets/riya_harchandaniLogo2.png";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
         <a href="/" aria-label="Home">
-            <img src={logo} className="mx-2" width={50} height={33}
-            alt="Logo" />
+          <img src={logo} className="mx-2" width={50} height={33} alt="Logo" />
         </a>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <a href="https://www.linkedin.com/in/your-linkedin-profile"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="LinkedIn">
-               <FaLinkedinIn />
+        <a
+          href="https://www.linkedin.com/in/riya-harchandani-09b5992b7/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
+          <FaLinkedinIn />
         </a>
-        <a href="https://www.github.com/in/your-github-profile"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Github">
-               <FaGithub />
+        <a
+          href="https://www.github.com/in/https://github.com/riyaharchandani"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Github"
+        >
+          <FaGithub />
         </a>
-        <a href="https://www.instagram.com/in/your-instagram-profile"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Instagram">
-               <FaInstagram />
+        <a
+          href="https://www.instagram.com/in/your-instagram-profile"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Instagram"
+        >
+          <FaInstagram />
         </a>
-        <a href="https://www.twitter.com/in/your-twitter-profile"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Twitter">
-               <FaSquareXTwitter />
+        <a
+          href="https://www.twitter.com/in/your-twitter-profile"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Twitter"
+        >
+          <FaSquareXTwitter />
         </a>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
