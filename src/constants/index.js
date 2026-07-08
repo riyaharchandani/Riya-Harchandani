0b8097c161347d3c40fa61fe3786a99a27a51f8c
riyaharchandani <riyaharchandani76@gmail.com>
2026-07-08 14:52:32 +0530

    edited

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ import project2 from "../assets/projects/project-2.webp";
 import project3 from "../assets/projects/project-3.webp";
 import project4 from "../assets/projects/project-4.webp";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like Javascript and React, as well as back-end technologies like Node.js, MySQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate Full Stack Developer skilled in creating responsive and scalable web applications. I specialize in frontend technologies like React.js, JavaScript, HTML, CSS, and Tailwind CSS, with knowledge of backend development using Node.js, Express.js, and databases like MongoDB and MySQL. I enjoy building practical solutions, exploring new technologies, and continuously improving my development and problem-solving skills.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications.I have worked with a variety of technologies, including React, Node.js, MySQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
